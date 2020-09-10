eab8b3b4ce98c878258a34fa8dce12952c525111
ifaizan <faizan.sysops@gmail.com>
2020-09-10 17:14:06 +0500

    set localstorage and adjusted ui

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const weather = new Weather('karachi');
-const ui = new UI;
+const ui = new UI();
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+
+const weather = new Weather(weatherLocation.city);
+
+document.addEventListener('DOMContentLoaded', getWeather);
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    weather.changeLocation(city);
+
+    storage.setLocationData(city);
+
+    getWeather();
+    $('#locModal').modal('hide');
+    // document.getElementById('city').value = '';
+})
 
 // weather.changeLocation('');
 
-weather.getWeather()
+
+function getWeather() {
+    weather.getWeather()
     .then( (weatherData) => {
         ui.paint(weatherData);
     })
     .catch(err => console.log(err));
+}
